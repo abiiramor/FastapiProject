a56f4d6159a84973c2d134dfa5c567145c7608f1
abiiramor <abir.benaamor@gmail.com>
2023-07-26 22:39:15 +0100

    Schema Validation with Pydantic

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title : str
+    content: str
+    #optional field for our schema
+    published: bool= True
+    #default nan 
+    rating: Optional[int] = None
+
+
+
 
 #requset Get method url :"/ " 
 
@@ -14,6 +27,8 @@ def get_posts():
     return {"data":"this is a post for testing"}
 
 @app.post("/createposts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return{"new post":f"title:{payload['title']} content{payload['content']}"}
+def create_post(post:Post):
+    print(post)
+    print(post.dict())
+    return{"data" : post}
+
