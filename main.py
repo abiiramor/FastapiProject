e069f20cb7246d293d6bd3aede0043f773a2825e
abiiramor <abir.benaamor@gmail.com>
2023-07-27 21:33:49 +0100

    CRUD operations with memory array

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -13,22 +14,25 @@ class Post(BaseModel):
     #default nan 
     rating: Optional[int] = None
 
-
+#create variable to save post into array
+my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1},
+{"title":"favorite drink", "content":"coffee", "id":3}]
 
 
 #requset Get method url :"/ " 
 
 @app.get("/")
 def root(): # path operation function 
-    return {"Hello": "fast api project"}
+    return {"Hello abir ": "fast api project"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"this is a post for testing"}
+    return {"data":my_posts }
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_post(post:Post):
-    print(post)
-    print(post.dict())
-    return{"data" : post}
+    post_dict =post.dict()
+    post_dict['id']= randrange(0 , 100000)
+    my_posts.append(post_dict)
+    return{"data" : post_dict}
 
